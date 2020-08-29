76e533f5186b101f9bded585bcfe0dcf2f7ed1ed
Alan Renner <renner.131@osu.edu>
2020-08-29 09:42:34 -0700

    Alan Renner Solution

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '301131284181459',
-        clientSecret: '0153805a4ee7d31c1dbdff22992417ab'
-    }
-}
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/nucampsite",
+};
+
+module.exports = {
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "311044733298901",
+    clientSecret: "7c4f0bb6830386aaf43d51bc73e8c049",
+  },
+};
